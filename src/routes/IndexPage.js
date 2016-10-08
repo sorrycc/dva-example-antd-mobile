55bb289711f8c9f807aff7fccd633b9be4369175
sorrycc <sorrycc@gmail.com>
2016-10-08 08:28:00 +0800

    :ghost:

---
+++ src/routes/IndexPage.js
@@ -0,0 +1,24 @@
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'dva';
+import { Link } from 'dva/router';
+import styles from './IndexPage.less';
+import { Button } from 'antd-mobile';
+
+function IndexPage() {
+  return (
+    <div className={styles.normal}>
+      <Button>Start</Button>
+      <h1>Welcome to dva!</h1>
+      <hr />
+      <ul className={styles.list}>
+        <li>To get started, edit <code>src/index.js</code> and save to reload.</li>
+        <li><a href="https://github.com/sorrycc/blog/issues/8" target="_blank">Getting Started</a></li>
+      </ul>
+    </div>
+  );
+}
+
+IndexPage.propTypes = {
+};
+
+export default connect()(IndexPage);
