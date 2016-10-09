ad3fb3185bafea234dfc91c22b310421232b543b
sorrycc <sorrycc@gmail.com>
2016-10-09 14:50:02 +0800

    add Tabs

--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -2,18 +2,36 @@ import React, { Component, PropTypes } from 'react';
 import { connect } from 'dva';
 import { Link } from 'dva/router';
 import styles from './IndexPage.less';
-import { Button } from 'antd-mobile';
+import { Tabs } from 'antd-mobile';
+
+const TabPane = Tabs.TabPane;
 
 function IndexPage() {
   return (
     <div className={styles.normal}>
-      <Button>Start</Button>
-      <h1>Welcome to dva!</h1>
-      <hr />
-      <ul className={styles.list}>
-        <li>To get started, edit <code>src/index.js</code> and save to reload.</li>
-        <li><a href="https://github.com/sorrycc/blog/issues/8" target="_blank">Getting Started</a></li>
-      </ul>
+      <Tabs defaultActiveKey="1" onChange={(key) => { console.log(key) }}>
+        <TabPane tab="选项卡一" key="1">
+          <div style={{
+            display: 'flex', alignItems: 'center', justifyContent: 'center', height: 100,
+          }}>
+             选项卡一内容
+          </div>
+        </TabPane>
+        <TabPane tab="选项卡二" key="2">
+          <div style={{
+            display: 'flex', alignItems: 'center', justifyContent: 'center', height: 100,
+          }}>
+             选项卡二内容
+          </div>
+        </TabPane>
+        <TabPane tab="选项卡三" key="3">
+          <div style={{
+            display: 'flex', alignItems: 'center', justifyContent: 'center', height: 100,
+          }}>
+             选项卡三内容
+          </div>
+        </TabPane>
+      </Tabs>
     </div>
   );
 }
